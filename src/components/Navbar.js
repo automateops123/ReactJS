f06fad76e3213babaddc8dac9684fb3746079697
Sai <narendrasairamp@gmail.com>
2021-06-23 11:00:54 +0000

    Navbar.js edited online with Bitbucket

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from './Button';
+// import { Button } from './Button';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
   const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
+ // const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
-
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
 
   useEffect(() => {
     showButton();
